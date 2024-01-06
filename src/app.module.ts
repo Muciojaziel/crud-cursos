f14f4a931d4459f0c40b1356c1f7881e698cdf76
Mucio Jaziel <muciojaziel@hotmail.com>
2024-01-06 10:29:45 -0300

    feat: configurações ORM e ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CursosModule } from './cursos/cursos.module';
 
 @Module({
-  imports: [CursosModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.dev'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/migration/{.ts,*.js}`],
+      migrationsRun: true,
+      synchronize: true, // P.s: atributo que não deve ir à prod
+    }),
+    CursosModule,
+  ],
   controllers: [],
   providers: [],
 })
