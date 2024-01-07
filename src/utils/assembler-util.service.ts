5aa4a9a9f266d6b4c90715374d70f6a15c968a70
Mucio Jaziel <muciojaziel@hotmail.com>
2024-01-06 22:35:40 -0300

    refact: ajustes de configurações

--- src/utils/assembler-util.service.ts
+++ src/utils/assembler-util.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AssemblerUtil {
+export class AssemblerUtilService {
   public mapDtoToEntity<T>(entity: T, dto: Partial<T>): T {
     Object.keys(dto).forEach((key) => {
       if (dto[key] !== undefined) {
